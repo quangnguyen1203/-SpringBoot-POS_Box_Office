8a6d827ea8f1909aed1a121af3738b4d706f212b
pgtc97 <79627755+pgtc97@users.noreply.github.com>
2021-07-29 13:52:35 +0700

    update

---
+++ src/main/java/com/cg/bo/model/User.java
@@ -0,0 +1,37 @@
+package com.cg.bo.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class User extends BaseEntity {
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String fullName;
+
+    @ManyToOne
+    @JoinColumn(name = "role_id")
+    private Role role;
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", fullName='" + fullName + '\'' +
+                ", roles=" + role +
+                '}';
+    }
+}
